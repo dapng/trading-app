b9381cc6bd59c06d38d26caa7a5ec2aa890623a2
Danila <41669874+dapng@users.noreply.github.com>
2023-04-03 15:06:26 +0300

    add test endpoints

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from enum import Enum
+from typing import List, Optional
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import ValidationError
+from fastapi.responses import JSONResponse
 
 app = FastAPI(
     title="Trading app"
 )
+
+
+@app.exception_handler(ValidationError)
+def validation_exception_handler(request: Request, exc: ValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors()}),
+    )
 
 
 fake_user = [
@@ -12,9 +28,6 @@ fake_user = [
 ]
 
 
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
-    return [user for user in fake_user if user.get("id") == user_id]
 
 
 fake_trades = [
@@ -32,7 +45,33 @@ fake_user2 = [
     {"id": 1, "role": "admin", "name": "Bob"},
     {"id": 2, "role": "investor", "name": "John"},
     {"id": 3, "role": "trader", "name": "Matt"},
+     {"id": 4, "role": "investor", "name": "Homer", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
+    ]},
 ]
+
+
+class DegreeType(Enum):
+    newbie = "newbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] = []
+
+
+@app.get("/users/{user_id}", response_model=List[User])
+def get_user(user_id: int):
+    return [user for user in fake_user if user.get("id") == user_id]
 
 
 @app.post("/users/{user_id}")
@@ -40,3 +79,18 @@ def change_useer_name(user_id: int, new_name: str):
     current_user = list(filter(lambda user: user.get("id") == user_id, fake_user2))[0]
     current_user["name"] = new_name
     return {"status": 200, "data": current_user}
+
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str
+    side: str
+    price: float = Field(ge=0)
+    amount: float
+
+
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
