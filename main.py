ee342ec1ca19f17df6d4d48654439867852f97b2
Danila <41669874+dapng@users.noreply.github.com>
2023-04-03 12:16:54 +0300

    add sample proj

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Trading app"
+)
+
+
+fake_user = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+]
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return [user for user in fake_user if user.get("id") == user_id]
+
+
+fake_trades = [
+    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
+    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
+]
+
+
+@app.get("/trades/")
+def get_trades(limit: int, offset: int):
+    return fake_trades[offset:][:limit]
+
+
+fake_user2 = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+]
+
+
+@app.post("/users/{user_id}")
+def change_useer_name(user_id: int, new_name: str):
+    current_user = list(filter(lambda user: user.get("id") == user_id, fake_user2))[0]
+    current_user["name"] = new_name
+    return {"status": 200, "data": current_user}
